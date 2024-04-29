acba4885edbdf82f8af587fdd28f54d24a380a25
Mostafa Khairy <139318986+Mostafa-Khairy0@users.noreply.github.com>
2024-04-29 09:40:38 +0300

    Update search.ts

--- client/src/api/search.ts
+++ client/src/api/search.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-axios.defaults.baseURL = `http://localhost:8000`;
+axios.defaults.baseURL = `https://wiki-search-engine.onrender.com`;
 
 export default function search(text: string) {
   return axios
