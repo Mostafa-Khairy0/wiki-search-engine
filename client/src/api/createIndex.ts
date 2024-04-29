1747b135e2770d5b1333e6a8c1238153ae154a7d
Mostafa Khairy <139318986+Mostafa-Khairy0@users.noreply.github.com>
2024-04-29 09:40:23 +0300

    Update createIndex.ts

--- client/src/api/createIndex.ts
+++ client/src/api/createIndex.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-axios.defaults.baseURL = `http://localhost:8000`;
+axios.defaults.baseURL = `https://wiki-search-engine.onrender.com`;
 
 export default function createIndex(type: string, preprocess: string[]) {
   return axios.post(`/createIndex/${type}`, { preprocess }).catch((err) => {
